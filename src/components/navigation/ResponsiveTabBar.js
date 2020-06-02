89895ad84a18ed56d02254c2ed5f13e0299554bf
kayewrobleski <wrobleski.kaye@gmail.com>
2020-06-02 14:32:26 -0500

    update nav bar to use media queries

--- src/components/navigation/ResponsiveTabBar.js
+++ src/components/navigation/ResponsiveTabBar.js
@@ -1,47 +1,48 @@
 import React  from 'react'
-import WebBanner from './WebBanner';
-import MobileBanner from './MobileBanner';
-import MobileTabBar from './MobileTabBar';
+import styled from 'styled-components'
+import WebBanner from './WebBanner'
+import MobileBanner from './MobileBanner'
+import MobileTabBar from './MobileTabBar'
 
 import theme from '../../styles/themes/default'
 
+const Wrapper = styled.div`
+    &.web-tabbar {
+        ${props => props.theme.breakpoints.down('xs')} {
+            display: none;
+        }
+    }
+    
+    &.mobile-tabbar {
+        ${props => props.theme.breakpoints.up('sm')} {
+            display: none;
+        }
+    }
+`
+
+Wrapper.defaultProps = {
+    theme: theme
+}
+
 const ResponsiveTabBar = (props) => {
-    const { theme, selected, children } = props;
-    const [vertical, setVertical] = React.useState(props.vertical);
+    const { selected, children } = props;
     const [show, setShow] = React.useState(false);
     
     const handleClick = () => {
         setShow(!show);
     }
 
-    React.useEffect(() => {
-        function handleResize() {
-            const size = window.innerWidth;
-            if (size < theme.breakpoints.values.sm && !vertical) {
-                setVertical(true);
-            } else if (size >= theme.breakpoints.values.sm && vertical) {
-                setVertical(false);
-            }
-
-        }
-        window.addEventListener('resize', handleResize);
-        return () => window.removeEventListener('resize', handleResize);
-    })
-
-    const TabBar = () => {
-        if (vertical) {
-            return (
-                <div>
+    return (
+        <div>
+            <Wrapper className="mobile-tabbar">
                 <MobileBanner menuClick={handleClick} />
                 <MobileTabBar selected={selected} show={show}>{ children }</MobileTabBar>
-                </div>
-            )
-        } else {
-            return <WebBanner selected={selected}>{ children }</WebBanner>
-        }
-    }
-
-    return <TabBar />
+            </Wrapper>
+            <Wrapper className="web-tabbar">
+                <WebBanner selected={selected}>{ children }</WebBanner>
+            </Wrapper>
+        </div>
+    )
 }
 
 ResponsiveTabBar.defaultProps = {
